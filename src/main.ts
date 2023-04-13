9c9b0e91131765d30ebabd2042e600c05747883c
devInnow8 <devInnow8@gmail.com>
2023-04-13 15:46:15 +0530

    changes updated

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+// import keepAlive from "../alive";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Accept",
   });
   await app.listen(3000);
+  // keepAlive();
 }
 bootstrap();
